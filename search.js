a90e69b97d23a62d951dc53c5ec29114c0c40508
areshaistg <areshaistg@gmail.com>
2023-02-19 00:06:32 +0800

    link detection with regex and animation

--- search.js
+++ search.js
@@ -1,4 +1,5 @@
-let input = document.getElementById("searchbar");
+const input = document.getElementById("searchbar");
+const linkAlert = document.getElementById("link-alert");
 
 function isValidHttpUrl(string) {
 	let url;
@@ -7,20 +8,25 @@ function isValidHttpUrl(string) {
 	} catch (_) {
 		return false;
 	}
-
 	return url.protocol === "http:" || url.protocol === "https:";
 }
 
+function looksLikeALink(str) {
+	let match = str.match(/\b((?=[a-z0-9-]{1,63}\.)(xn--)?[a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,63}\b/gm);
+	if (match) {
+		return match[0] === str;
+	}
+	return false;
+}
 
 function search() {
-	let query = input.value;
+	const query = input.value;
 	// input = input.toLowerCase(); // might remove later
 	if (query !== "") {
 		let url = isValidHttpUrl(query);
 		if (url === false) {
-			let edited = "https://" + query;
-			if (isValidHttpUrl(edited)) {
-				window.location.href = edited;
+			if (looksLikeALink(query)) {
+				window.location.href = "https://" + query;
 			} else {
 				let modif = query.replace(/\s+/g, '+');
 				window.location.href = "https://google.com/search?q=" + modif;
@@ -31,10 +37,18 @@ function search() {
 	}
 }
 
-input.addEventListener("keypress", (event) => {
+const defaultLinkAlertDisplay = linkAlert.style.display;
+
+input.addEventListener("keyup", (event) => {
 	if (event.key === "Enter") {
 		event.preventDefault();
 		search();
+	} else {
+		if (looksLikeALink(input.value)) {
+			linkAlert.classList.add("active");
+		} else {
+			linkAlert.classList.remove("active");
+		}
 	}
 })
 
